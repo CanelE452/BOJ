3fa0ec894b0f941c504393c7e4fc73adf85a6a89
CanelE452 <153896450+CanelE452@users.noreply.github.com>
2024-01-15 17:20:11 +0900

    Update 빗물.cc

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,3 +1,28 @@
+// 투포인터를 이용해서 시간복잡도 줄일수 있
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int H,W,ans=0;
+
+// int main() {
+//     cin>>H>>W;
+//     vector<int> v(W);
+
+//     for(int i=0;i<W;i++) cin>>v[i];
+
+//     for(int i=1;i<W-1;i++){
+//         int left=0,right=0;
+//         for(int j=0;j<i;j++) left=max(left,v[j]);
+//         for (int j = W - 1; j > i; j--) right = max(right, v[j]);
+//         ans+=max(0,min(left,right)-v[i]);
+//     }
+//     cout<<ans;
+// }     
+
+
+
 #include <iostream>
 
 using namespace std;
