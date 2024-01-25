ad8c0f3c208b43887d4bed7df802599285781c18
CanelE452 <153896450+CanelE452@users.noreply.github.com>
2024-01-25 15:23:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 104 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define INF 987654321 
+#define MAX_N 1001
+
+typedef pair<int,int>pii;
+
+int graph[MAX_N][MAX_N];
+int dist[MAX_N];
+int city, bus;
+
+void dijkstra(int start){
+    priority_queue <pii,vector<pii>,greater<pii>> pq;
+    bool visited[MAX_N]={false};
+    for(int i=1;i<=city;i++) dist[i]=INF; dist[start]=0;
+    pq.push({0,start});
+
+    while(!pq.empty()){
+        int cur=pq.top().second;
+        pq.pop();
+
+        if(visited[cur]) continue;  visited[cur]=true;
+
+        for(int v=1;v<=city;v++){
+            if(dist[v]>dist[cur]+graph[cur][v]){
+                dist[v]=dist[cur]+graph[cur][v];
+                pq.push({dist[v],v});
+            }
+        }
+    }
+}
+
+int main(){
+    cin>>city;
+    cin>>bus;
+
+    for(int i=1;i<=city;i++){
+        for(int j=1;j<=city;j++){
+            if(i==j) graph[i][j]=0;
+            else graph[i][j]=INF;
+        }
+    }
+
+    while(bus--){
+        int start,end,cost; cin>>start>>end>>cost;
+        if(graph[start][end]>cost) graph[start][end]=cost;
+    }
+
+    int dept,dest; cin>>dept>>dest;
+    dijkstra(dept);
+    cout<<dist[dest];
+}
+
