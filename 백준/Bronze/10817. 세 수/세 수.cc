a032356641206c837df0ddd4c310bb70c3c41516
CanelE452 <153896450+CanelE452@users.noreply.github.com>
2024-02-05 17:30:50 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n; 
+    vector<int> v;
+    for(int i=0;i<3;i++){
+        cin>>n;
+        v.push_back(n);
+    }
+    sort(v.begin(),v.end());
+    cout<<v[1];
+}
