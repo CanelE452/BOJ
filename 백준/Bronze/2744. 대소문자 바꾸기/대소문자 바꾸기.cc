c6a828d0512dada12962adb74387195f52c6b60c
CanelE452 <153896450+CanelE452@users.noreply.github.com>
2024-02-26 18:22:25 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -1,16 +1,16 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-
 int main(){
-    string s;
-    cin>>s;
-    for(int i=0;i<s.size();i++){
-        if(s[i]>='a' && s[i]<='z')
-            cout<<char(s[i]-('a'-'A'));       
-        else
-            cout<<char(s[i]+('a'-'A'));
-    }
+    string str; cin>>str;
 
+    for(int i=0;i<str.size();i++){
+        if(islower(str[i])!=0) str[i] = toupper(str[i]);
+        else str[i] = tolower(str[i]);
+    }
+    
+    cout<<str;
 }
