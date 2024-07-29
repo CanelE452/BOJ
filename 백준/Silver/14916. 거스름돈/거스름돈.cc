7152daef44fbe1d13d7d70eca90587bc94563c85
CanelE452 <153896450+CanelE452@users.noreply.github.com>
2024-07-29 20:18:07 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int dp[100001];
+    dp[0] = -1;
+    dp[1] = -1;
+    dp[2] = 1;
+    dp[3] = -1;
+    dp[4] = 2;
+    dp[5] = 1;
+    dp[6] = 3;
+    dp[7] = 2;
+    dp[8] = 4; // 초기값 정리
+    for (int i = 9; i <= n;i++){
+        dp[i] = dp[i - 5] + 1; // 5개 단위로 한 개씩 늘어남 
+    }
+    cout << dp[n];
+    return 0;
+}
